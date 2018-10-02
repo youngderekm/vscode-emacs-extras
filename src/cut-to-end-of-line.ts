ca0301e6ceb4af10d73b6cd19f2c566d93447608
Derek Young <dmy999@gmail.com>
2018-10-02 10:50:52 -0400

    Correctly cut when cursor is at end of line.

--- src/cut-to-end-of-line.ts
+++ src/cut-to-end-of-line.ts
@@ -10,10 +10,19 @@ export default function () {
     // selection from where we are to the end of the line.
     const position = editor.selection.active;
     const endOfLine = editor.document.lineAt(position.line).range.end;
-    const selection = new vscode.Selection(position, endOfLine);
+    let selection = new vscode.Selection(position, endOfLine);
     // if anything is selected, cut it to the clipboard
     if (!selection.isEmpty) {
         editor.selection = selection;
         vscode.commands.executeCommand("editor.action.clipboardCutAction");
+    } else {
+        // cursor is at end of line, so combine it with the next line.
+        const beginningOfNextLine = endOfLine.translate(1).with(undefined, 0);
+
+        if (beginningOfNextLine.line < editor.document.lineCount) {
+            // not at the final line of the document, so OK to cut
+            selection = new vscode.Selection(position, beginningOfNextLine);
+            vscode.commands.executeCommand("editor.action.clipboardCutAction");
+        }
     }
 }
